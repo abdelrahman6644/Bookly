13f76e32ed859447604be5ad54eff6537b097fab
abdelrahman6644 <abdalrahmankhaled642004@gmail.com>
2024-09-29 10:09:56 +0300

    create customAppBar in HomeView

---
+++ lib/Features/splash/home/data/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:bookly_app/core/utls/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            Assets.logo,
+            height: 20,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
