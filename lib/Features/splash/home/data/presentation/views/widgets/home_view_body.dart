5fa65da805d1fb79690d8f7b216f1772b292c387
abdelrahman6644 <abdalrahmankhaled642004@gmail.com>
2024-10-01 11:28:54 +0300

    best seller list item part 1

--- lib/Features/splash/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/splash/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/splash/home/data/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/splash/home/data/presentation/views/widgets/custom_list_view.dart';
+import 'package:bookly_app/core/utls/assets.dart';
 import 'package:bookly_app/core/utls/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +20,34 @@ class HomeViewBody extends StatelessWidget {
             height: 20,
           ),
           Text("Best Saller", style: Styles.titleMedium),
+          BestSellerListViewItem()
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 110,
+          child: Row(
+            children: [
+              ShowPoster(
+                Imageurl: Assets.test_image,
+                height: 110,
+                width: 70,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/core/utls/styles.dart
+++ lib/core/utls/styles.dart
@@ -4,5 +4,6 @@ abstract class Styles {
   static const titleMedium = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w600,
+    
   );
 }
