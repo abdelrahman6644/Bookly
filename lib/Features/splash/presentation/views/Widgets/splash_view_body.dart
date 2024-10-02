82d5f0d579d8e0c8323a31797bae3b3146f69a3e
abdelrahman6644 <abdalrahmankhaled642004@gmail.com>
2024-10-02 09:45:17 +0300

    refactor navigation

--- lib/Features/splash/presentation/views/Widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/Widgets/splash_view_body.dart
@@ -1,10 +1,8 @@
-import 'package:bookly_app/Features/splash/home/data/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/Widgets/animated_text.dart';
-import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utls/app_router.dart';
 import 'package:bookly_app/core/utls/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
+import 'package:go_router/go_router.dart';
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
@@ -15,16 +13,15 @@ class SplashViewBody extends StatefulWidget {
 class _SplashViewBodyState extends State<SplashViewBody> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     navigateToHome();
   }
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 3), () {
-      Get.to(() => const HomeView(),
-          transition: Transition.fade, duration: kTranstionDuration);
-    });
+    GoRouter.of(context).push(AppRouter.kHomeView);
+    }
+    );
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utls/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
         textTheme: GoogleFonts.montserratTextTheme(
@@ -21,7 +23,7 @@ class BooklyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashView(),
+      // home: const SplashView(),
     );
   }
 }
